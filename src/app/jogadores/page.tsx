75340db948a04ca642b5686076a1672da72e60c4
GuillyCosta <costa.guilly@gmail.com>
2025-05-07 18:16:28 -0300

    Update page.tsx

--- src/app/jogadores/page.tsx
+++ src/app/jogadores/page.tsx
@@ -51,16 +51,9 @@ function GameApp() {
 // Página principal que envolve o aplicativo com o provedor de contexto
 export default function Home() {
   return (
-    <> {/* <--- Abre um React Fragment */}
-      <GameProvider>
-        <GameApp />
-      </GameProvider>
-      <div> {/* Agora este div está dentro do fragmento */}
-        <footer className="mt-12 text-center text-gray-500 text-sm">
-            <p>© 2025 TasKing - Todos os direitos reservados</p>
-          </footer>
-      </div>
-    </> {/* <--- Fecha o React Fragment */}
+    <GameProvider>
+      <GameApp />
+    </GameProvider>
   );
 }
 
